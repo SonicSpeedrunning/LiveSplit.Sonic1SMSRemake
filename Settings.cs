14b6491c9cbeb74861e23a19989f18f2cf3d8822
Jujstme <just.tribe@gmail.com>
2023-01-13 12:52:04 +0100

    Fixed small oversight with the default splitting settings

--- Settings.cs
+++ Settings.cs
@@ -77,7 +77,7 @@ namespace LiveSplit.Sonic1SMSRemake
 
             // Default Values
             Start = Reset = true;
-            c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = c10 = c11 = c12 = c13 = c14 = c15 = c16 = c17 = c18 = c19 = c20 = c21 = c22 = c23 = c24 = c25 = c26 = true;
+            c0 = c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = c10 = c11 = c12 = c13 = c14 = c15 = c16 = c17 = c18 = c19 = c20 = c21 = c22 = c23 = c24 = c25 = c26 = true;
         }
 
         public XmlNode GetSettings(XmlDocument doc)
